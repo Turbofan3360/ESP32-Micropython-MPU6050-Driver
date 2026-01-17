845339ffbeff40d83408aa7726e131a7b20b65ac
Samuel Pontefract <zia32kvs@duck.com>
2026-01-17 18:22:40 +0100

    Added interrupt handling + full MPU6050 & DMP setup

--- embedded_c_module/mpu6050.c
+++ embedded_c_module/mpu6050.c
@@ -1,11 +1,16 @@
 #include "mpu6050.h"
 
+// Creating a variable to hold whether an interrupt has been triggered
+// 'volatile' meaning data can change outside of normal program flow
+static volatile uint8_t sensor_data_ready = 0;
+
 mp_obj_t mpu6050_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args){
     /**
      * This function initialises a new driver instance. It initialises the I2C bus and adds the IMU to it
+     * Configures the interrupt pin for MPU6050
      * It then calls imu_setup to configure the MPU6050's config registers as required.
     */
-    gpio_num_t scl_pin, sda_pin;
+    gpio_num_t scl_pin, sda_pin, int_pin;
     int8_t port;
     i2c_port_num_t i2c_port;
     i2c_master_bus_handle_t bus_handle;
@@ -16,6 +21,7 @@ mp_obj_t mpu6050_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw,
     static const mp_arg_t allowed_args[] = {
         { MP_QSTR_scl, MP_ARG_REQUIRED | MP_ARG_INT },
         { MP_QSTR_sda, MP_ARG_REQUIRED | MP_ARG_INT },
+        { MP_QSTR_int, MP_ARG_REQUIRED | MP_ARG_INT },
         { MP_QSTR_i2c_port, MP_ARG_KW_ONLY | MP_ARG_INT, {.u_int = DEFAULT_I2C_PORT_NUM} },
     };
 
@@ -26,11 +32,12 @@ mp_obj_t mpu6050_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw,
     // Extracting arguments
     scl_pin = parsed_args[0].u_int;
     sda_pin = parsed_args[1].u_int;
-    port = parsed_args[2].u_int;
+    int_pin = parsed_args[2].u_int;
+    port = parsed_args[3].u_int;
 
     // Ensuring I2C port number and pin numbers are valid
-    if (!GPIO_IS_VALID_OUTPUT_GPIO(scl_pin) || !GPIO_IS_VALID_OUTPUT_GPIO(sda_pin)){
-        mp_raise_msg(&mp_type_ValueError, MP_ERROR_TEXT("Invalid SCL or SDA pin number"));
+    if (!GPIO_IS_VALID_OUTPUT_GPIO(scl_pin) || !GPIO_IS_VALID_OUTPUT_GPIO(sda_pin) || !GPIO_IS_VALID_OUTPUT_GPIO(int_pin)){
+        mp_raise_msg(&mp_type_ValueError, MP_ERROR_TEXT("Invalid SCL/SDA/interrupt pin number"));
     }
 
     if ((port < -1) || (port > 1)){
@@ -88,6 +95,24 @@ mp_obj_t mpu6050_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw,
         mp_raise_msg_varg(&mp_type_RuntimeError, MP_ERROR_TEXT("Error adding device to I2C bus: %s"), esp_err_to_name(err));
     }
 
+    // Installing ISR handler
+    gpio_install_isr_service(0);
+
+    // Configuring interrupt pin
+    gpio_config_t int_pin_config = {
+        .pin_bit_mask = 1ULL<<int_pin,
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = GPIO_PULLUP_ENABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_NEGEDGE,
+    };
+
+    // Installing that config onto the GPIO pin
+    gpio_config(&int_pin_config);
+
+    // Registering ISR handler
+    gpio_isr_handler_add(int_pin, interrupt_handler, NULL);
+
     // Creating the "self" object for this driver
     mpu6050_obj_t* self = m_new_obj(mpu6050_obj_t);
 
@@ -97,8 +122,6 @@ mp_obj_t mpu6050_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw,
     self->device_handle = device_handle;
     self->i2c_address = MPU6050_I2C_ADDRESS;
 
-
-
     // 1ms delay to ensure the chip powers up properly
     wait_micro_s(1000);
 
@@ -107,7 +130,143 @@ mp_obj_t mpu6050_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw,
     return MP_OBJ_FROM_PTR(self);
 }
 
-static void imu_setup(mpu6050_obj_t *self){
+static void IRAM_ATTR interrupt_handler(void* arg){
+    /**
+     * Interrupt handler for the MPU6050 - just sets a flag to know that data is available
+    */
+    sensor_data_ready = 1;
+
+    return;
+}
+
+static void write_to_register(mpu6050_obj_t* self, uint8_t reg, uint8_t data, char* error_text){
+    /**
+     * Utility to enable easy writes to MPU6050 config registers
+     * Only writes single bytes of data
+    */
+    uint8_t write_data[2] = {reg, data};
+
+    // Writing data
+    err = i2c_master_transmit(self->device_handle, write_data, 2, 100);
+
+    if (err != ESP_OK){
+        mp_raise_msg(&mp_type_RuntimeError, MP_ERROR_TEXT(error_text));
+    }
+}
+
+static void read_from_register(mpu6050* self, uint8_t reg, uint8_t* output, char* error_text){
+    /**
+     * Utility to read 1 byte from a specific MPU6050 register
+    */
+    uint8_t write[1] = {reg};
+
+    // Reading data
+    err = i2c_master_transmit_receive(self->device_handle, write_whi, 1, output, 1, 20);
+
+    if (err != ESP_OK){
+        mp_raise_msg(&mp_type_RuntimeError, MP_ERROR_TEXT(error_text));
+    }
+}
+
+static void write_dmp_firmware(mpu6050_obj_t* self){
+    /**
+     * Function to write the firmware to the MPU6050's DMP
+    */
+    uint8_t bank, fw_byte, readback_fw_byte;
+    uint16_t byte_offset, limit;
+
+    // Iterating through firmware banks
+    for (bank = 0; bank < NO_FW_BANKS - 1; bank++){
+        // Writing firmware bank number
+        write_to_register(self, DMP_CTRL_1, bank, "Failed to write DMP firmware bank number");
+
+        // Setting up variables to write the bank data
+        byte_offset = 0;
+
+        if (bank == 11){
+            limit = 246;
+        }
+        else {
+            limit = 256;
+        }
+
+        // Writing bank data:
+        while (byte_offset < limit){
+            // Collecting firmware byte
+            fw_byte = dmp_firwave_v612[bank*256 + byte_offset];
+
+            // Writing firmware byte
+            write_to_register(self, DMP_CTRL_2, byte_offset, "Couldn't write DMP firmware byte offset");
+            write_to_register(self, DMP_CTRL_3, fw_byte, "Couldn't write DMP firmware byte");
+
+            // Reading back the firmware byte
+            write_to_register(self, DMP_CTRL_2, byte_offset, "Couldn't write DMP firmware byte offset");
+            read_from_register(self, DMP_CTRL_3, &readback_fw_byte, "Couldn't read back DMP firmware byte");
+
+            // Checking for errors: If none, then proceed
+            if (fw_byte == readback_fw_byte){
+                byte_offset ++;
+            }
+
+            // 1ms delay
+            wait_micro_s(1000)
+        }
+    }
+    return;
+}
+
+static void imu_setup(mpu6050_obj_t* self){
+    /**
+     * Writes to all the required MPU6050 registers to set up the module properly
+    */
+    uint8_t write_data[2];
+    esp_err_t err;
+
+    // Probing to check there's actually a device there
+    err = i2c_master_probe(self->bus_handle, self->i2c_address, 100);
+
+    if (err != ESP_OK){
+        mp_raise_msg_varg(&mp_type_RuntimeError, MP_ERROR_TEXT("MPU6050 device not found on I2C bus: %s"), esp_err_to_name(err));
+    }
+
+    // Setting DLPF to 42Hz (gyros) and 44Hz (accel)
+    write_to_register(self, CONFIG_REG, 0x03, "Unable to write to sensor configuration register");
+
+    // Setting sample rate to 200Hz
+    write_to_register(self, SAMPLERATE_DIV_REG, 0x04, "Unable to write to sensor configuration register");
+
+    // Enabling FIFO
+    write_to_register(self, FIFO_EN_REG, 0x00, "Unable to write to sensor configuration register");
+
+    // FIFO config
+    write_to_register(self, USER_CTRL_REG, 0x04, "Unable to write to sensor configuration register");
+
+    // Enabling Interrupts
+    write_to_register(self, INT_EN_REG, 0x00, "Unable to write to sensor configuration register");
+    write_to_register(self, INT_EN_REG, 0x02, "Unable to write to sensor configuration register");
+
+    // Configuring interrupts
+    write_to_register(self, INT_CONFIG_REG, 0x90, "Unable to write to sensor configuration register");
+
+    // Writing DMP firmware
+    write_dmp_firmware(self);
+
+    // Writing DMP start byte
+    write_to_register(self, DMP_FW_START_1, 0x04, "Unable to write to sensor configuration register");
+
+    // Enable and reset both DMP and FIFO
+    write_to_register(self, USER_CTRL_REG, 0xCC, "Unable to write to sensor configuration register");
+
+    // Reading from the 'WHO AM I' register
+    uint8_t read_whi;
+    read_from_register(self, WHOAMI_REG, &read_whi, "Couldn't read MPU6050 'WHO AM I' register: Unknown error during setup");
+
+    // Checking it's the expected response
+    if (read_whi != 0x68){
+        mp_raise_msg(&mp_type_RuntimeError, MP_ERROR_TEXT("I2C Connection Error - Bad return from 'MPU6050 WHO AM I' register"));
+    }
+
+    return;
 }
 
 
@@ -122,7 +281,7 @@ static void wait_micro_s(uint32_t micro_s_delay){
     return;
 }
 
-static void log_func(const char *log_string){
+static void log_func(const char* log_string){
     /**
      * Basic logging function - currently just prints to the REPL, but can be adapted to log to other places (e.g. log to a file) if needed
     */
